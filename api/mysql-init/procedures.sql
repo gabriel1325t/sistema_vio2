540e7b2f6516ede690abb9704d060b4c25600bb3
gabriel1325t <gabriel.carrasco1325@gmail.com>
2025-04-14 14:31:29 -0300

    14-04

--- api/mysql-init/procedures.sql
+++ api/mysql-init/procedures.sql
@@ -63,3 +63,69 @@ delimiter ;
 
 
 
+delimiter $$
+
+create procedure resumo_usuario(pid int)
+begin 
+    declare nome varchar(100);
+    declare email varchar(100);
+    declare totalrs decimal(10,2);
+    declare faixa varchar(20);
+
+  
+    select u.name, u.email into nome, email
+    from usuario u 
+    where u.id_usuario = pid;
+
+  
+
+    set totalrs = calcula_total_gasto(pid);
+    set faixa = buscar_faixa_etaria_usuario(pid);
+
+    
+    select nome as nome_usuario,
+            email as email_usuario,
+            totalrs as total_gasto,
+            faixa as faixa_etaria;
+        
+    end; $$
+delimiter ;
+
+delimiter $$
+
+DELIMITER $$
+
+create procedure resumo_evento(pid_evento int)
+begin
+    declare nome_evento varchar(100);
+    declare data_evento date;
+    declare total_ingressos int;
+    declare renda_total decimal(10,2);
+
+   
+    select e.nome, e.data_hora into nome_evento, data_evento
+    from evento e
+    where e.id_evento = pid_evento;
+
+    
+    select ifnull(sum(ic.quantidade), 0) into total_ingressos
+    from ingresso_compra ic
+    join ingresso i on i.id_ingresso = ic.fk_id_ingresso
+    where i.fk_id_evento = pid_evento;
+
+    
+    select ifnull(sum(i.preco * ic.quantidade), 0) into renda_total
+    from ingresso_compra ic
+    join ingresso i on i.id_ingresso = ic.fk_id_ingresso
+    where i.fk_id_evento = pid_evento;
+
+    
+    select nome_evento as nome_evento,
+           data_evento as data_evento,
+           total_ingressos as total_ingressos_vendidos,
+           renda_total as renda_total;
+end $$
+
+delimiter ;
+
+call resumo_evento(3); 
